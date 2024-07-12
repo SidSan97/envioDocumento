ea06c64f1380a6e3e30f8b247e31240922b3c6df
SidSan97 <sidnei1.8santiago@hotmail.com>
2024-07-12 08:10:55 -0300

    adição de novos arquivos

---
+++ front/src/stores/auth.ts
@@ -0,0 +1,43 @@
+import { defineStore } from 'pinia';
+import axios from 'axios';
+import { useRouter } from 'vue-router';
+
+export const useAuthStore = defineStore('auth', {
+    state: () => ({
+        token: localStorage.getItem('token') || null,
+        user: JSON.parse(localStorage.getItem('user')) || null
+    }),
+    actions: {
+        async login(email: string, password: string) {
+            const response = await axios.post('http://localhost/envioDocumento/backend/public/api/login', { email, password });
+            this.token = response.data.token;
+            this.user = response.data.user;
+
+            localStorage.setItem('token', this.token);
+            localStorage.setItem('user', JSON.stringify(this.user));
+
+            axios.defaults.headers.common['Authorization'] = `Bearer ${this.token}`;
+        },
+
+        async logout() {
+            if (this.token) {
+                try {
+                    await axios.post('http://localhost/envioDocumento/backend/public/api/logout', {}, {
+                        headers: {
+                            'Authorization': `Bearer ${this.token}`
+                        }
+                    });
+                } catch (error) {
+                    console.error('Erro ao fazer logout:', error.response);
+                }
+            }
+            this.token = null;
+            this.user = null;
+
+            localStorage.removeItem('token');
+            localStorage.removeItem('user');
+
+            delete axios.defaults.headers.common['Authorization'];
+        }
+    }
+});
---
+++ front/src/views/LoginView.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <img src="@/assets/LOGO.png" alt="" srcset="">
+
+        <div class="formLogin">
+            <form @submit.prevent="login">
+                <div class="form-group mb-4">
+                    <label for="exampleInputEmail1">Email</label>
+                    <input v-model="email" type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                </div>
+
+                <div class="form-group mb-4">
+                    <label for="exampleInputPassword1">Senha</label>
+                    <input v-model="password" type="password" class="form-control" id="exampleInputPassword1" placeholder="Senha">
+                </div>
+
+                <button type="submit" class="btn btn-primary">Login</button>
+            </form>
+
+            <div v-if="error" class="alert alert-danger mt-3">{{ error }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { useAuthStore } from '../stores/auth';
+import { useRouter } from 'vue-router';
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            error: '',
+        };
+    },
+    setup() {
+        const authStore = useAuthStore();
+        const router = useRouter();
+
+        if (authStore.token) {
+            router.push('/');
+        }
+
+        return { authStore, router };
+    },
+    methods: {
+        async login() {
+            try {
+                await this.authStore.login(this.email, this.password);
+                this.router.push('/');
+            } catch (err) {
+                this.error = 'Email ou senha incorretos.'; 
+            }
+        },
+    }
+};
+</script>
+
+<style scoped>
+    .formLogin{
+        width: 400px;
+    }
+</style>
